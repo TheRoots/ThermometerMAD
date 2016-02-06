9badd481fdcf911944dc78d2264e7f8597d408a3
Chris Statkiewicz <r1ot.rog@gmail.com>
2016-02-06 12:44:52 +0100

    commit page

--- js/header.js
+++ js/header.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
-    //sidebar hide/show
-    $('#menu').click(function () {
-        var hidden = $('#slider');
-        if (hidden.hasClass('visible')) {
-            hidden.animate({"right": "-350px"}, "medium").removeClass('visible');
-
-        } else {
-            hidden.animate({"right": "0px"}, "medium").addClass('visible');
-        }
-    });
-    
     // Hide Header on on scroll down
     var didScroll;
     var lastScrollTop = 0;
@@ -30,9 +19,6 @@ $(document).ready(function () {
         // If they scrolled down and are past the navbar, add class .nav-up.
         // This is necessary so you never see what is "behind" the navbar.
         if (st > lastScrollTop && st > navbarHeight) {
-            var isopen = $('#slider').hasClass('visible');
-            if (! isopen) {
-                // Scroll Down
             $('.header').removeClass('header-down').addClass('header-up');
             }
         } else {
